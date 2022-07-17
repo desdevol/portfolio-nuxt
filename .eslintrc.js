0895557c7564b7e25a72e89e3332cf6862696121
Desmond <ywlee9999@hotmail.com>
2022-07-18 05:03:12 +0800

    added eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ['@nuxtjs/eslint-config-typescript', 'plugin:nuxt/recommended', 'plugin:prettier/recommended'],
+
+    rules: {
+        'vue/multi-word-component-names': 0,
+        'prettier/prettier': ['error', { singleQuote: true, semi: false }],
+    },
+}
