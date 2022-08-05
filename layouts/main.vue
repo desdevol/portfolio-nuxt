39a601f702a1138669cb5cf24e7c327859e2d6d6
Desmond <ywlee9999@hotmail.com>
2022-08-05 14:48:13 +0800

    feat: updated main layout and remove Experience from nav

--- layouts/main.vue
+++ layouts/main.vue
@@ -3,20 +3,21 @@
 <template>
     <div>
         <!-- Topnav -->
-        <div class="navbar bg-base-100">
+        <div class="navbar fixed bg-base-100">
             <div class="flex-1">
                 <a class="btn btn-ghost btn-md text-xl normal-case">Desdevol</a>
             </div>
             <div class="flex-none">
                 <ul class="menu menu-horizontal p-0">
                     <NuxtLink to="/#about" class="btn btn-ghost btn-md mx-1 normal-case">About</NuxtLink>
-                    <NuxtLink to="/#experience" class="btn btn-ghost btn-md mx-1 normal-case">Experience</NuxtLink>
                     <NuxtLink to="/project" class="btn btn-ghost btn-md mx-1 normal-case">Projects</NuxtLink>
                     <NuxtLink to="/#contact" class="btn btn-ghost btn-md mx-1 normal-case">Contact</NuxtLink>
                 </ul>
             </div>
         </div>
-        <slot />
+        <div class="container mx-auto px-10">
+            <slot />
+        </div>
     </div>
 </template>
 
