ea69ba2d3e4d0cd513442e14b822180df504072d
Desmond <ywlee9999@hotmail.com>
2022-08-11 17:35:06 +0800

    feat: added fontawesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
-    css: ['@/assets/style.scss'],
+    css: ['@/assets/style.scss', '@fortawesome/fontawesome-svg-core/styles.css'],
     head: {
         title: 'Desdevol',
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
