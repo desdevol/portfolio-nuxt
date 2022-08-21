43670d46d884635736731a937917f344629fe293
Desmond <ywlee9999@hotmail.com>
2022-08-22 02:35:10 +0800

    chore: imported flicking css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,11 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt'],
-    css: ['@/assets/style.scss', '@fortawesome/fontawesome-svg-core/styles.css'],
+    css: [
+        '@/assets/style.scss',
+        '@fortawesome/fontawesome-svg-core/styles.css',
+        '@egjs/vue3-flicking/dist/flicking.css',
+    ],
     head: {
         title: 'Desdevol',
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
